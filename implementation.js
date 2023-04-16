719180c893f7bdd6ebc4827f93eb6ec22404233c
vvv <t.moll@outlook.com>
2023-04-16 16:57:57 -0400

    Enabling castlevania in the viewer

--- implementation.js
+++ implementation.js
@@ -11,16 +11,17 @@
 */
 
 // TODO: probably should use structs to contain this information
-var layerNames = ['kanto', 'interior', 'sevii'];
+var layerNames = ['kanto', 'interior', 'sevii', 'castlevania'];
 var activeLayerIndex = 0;           // Currently active layer index (and initial index)
 var activeAreas = kantoAreas          // Active array of areas (and initial area)
-var areaLayers = [kantoAreas, interiorAreas, seviiAreas];
+var areaLayers = [kantoAreas, interiorAreas, seviiAreas, castleAreas];
 
 // Canvases
 var kantoCanvas = {width: 5472, height: 5904}
 var interiorCanvas = {width: 5504, height: 5744}
 var seviiCanvas = {width: 4448, height: 6784}
-var layerCanvases = [kantoCanvas, interiorCanvas, seviiCanvas];
+var castleCanvas = {width: 10000, height: 2096}
+var layerCanvases = [kantoCanvas, interiorCanvas, seviiCanvas, castleCanvas];
 
 /** Dictionary pairing zone types to icon filenames. */
 let iconTypeDictionary = {
@@ -28,7 +29,12 @@ let iconTypeDictionary = {
     'forest' : 'park',
     'surfing' : 'surfing',
     'mountain' : 'landscape',
-    'route' : 'pedal_bike'
+    'route' : 'pedal_bike',
+
+    'Terrace' : 'landscape',
+    'Underground' : 'surfing',
+    'Castle' : 'park',
+    'Boss' : 'location_city',
 }
 
 /** Dictionary pairing zone types to rgb color definitions. */
@@ -37,5 +43,10 @@ let iconColorDictionary = {
     'forest' : 'rgb(94 130 105)',
     'surfing' : 'rgb(108 127 171)',
     'mountain' : 'rgb(130 115 88)',
-    'route' : 'rgb(110 130 88)'
+    'route' : 'rgb(110 130 88)',
+
+    'Terrace' : 'rgb(210 212 141)',
+    'Underground' : 'rgb(113 104 127)',
+    'Castle' : 'rgb(204 156 73)',
+    'Boss' : 'rgb(129 28 56)'
 }
