25f4a029c9eb666d3eafb6477cf6b1b87f7f902d
vvv <t.moll@outlook.com>
2023-10-09 23:11:01 -0400

    Removing Pokemon content.

--- implementation.js
+++ implementation.js
@@ -21,56 +21,11 @@ var redrawnLayers = [
         name: "castlevania",
         canvasSize: {width: 10000, height: 2096},
         areas: castleAreas
-    },
-    {
-        name: "kanto",
-        canvasSize: {width: 5472, height: 5904},
-        areas: kantoAreas
-    },
-    {
-        name: "interior",
-        canvasSize: {width: 5504, height: 5744},
-        areas: interiorAreas
-    },
-    {
-        name: "sevii",
-        canvasSize: {width: 4448, height: 6784},
-        areas: seviiAreas
-    },
+    }
 ];
 
 /** Biome Data (Screen icons) */
 var biomes = [
-    {
-        name: "Town",
-        ident: "town",
-        iconId: "location_city",
-        color: 'rgb(130 94 108)',
-    },
-    {
-        name: "Forest",
-        ident: "forest",
-        iconId: "park",
-        color: 'rgb(94 130 105)',
-    },
-    {
-        name: "Surfing",
-        ident: "surfing",
-        iconId: "surfing",
-        color: 'rgb(108 127 171)',
-    },
-    {
-        name: "Mountain",
-        ident: "mountain",
-        iconId: "landscape",
-        color: 'rgb(130 115 88)',
-    },
-    {
-        name: "Route",
-        ident: "route",
-        iconId: "pedal_bike",
-        color: 'rgb(110 130 88)',
-    },
     {
         name: "Terrace",
         ident: "terrace",
